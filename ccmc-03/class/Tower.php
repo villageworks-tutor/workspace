1b26b12f61843edfff126cc58e86ca3202607ab6
wpt1005 <wpt1005@ccg.ac.jp>
2019-11-10 02:10:34 +0900

    add tower class

---
+++ ccmc-03/class/Tower.php
@@ -0,0 +1,49 @@
+<?php
+/** 外部ファイルを読み込む */
+require_once "Building.php";
+?>
+<?php
+/**
+ * Towerクラス：タワーを管理するクラス
+ */
+class Tower extends Building {
+	/**
+	 * 属性：このクラスに特化した属性のみを宣言する。
+	 * @var int $builtYear 開業年
+	 */
+	private $builtYear;
+
+	/**
+	 * コンストラクタ
+	 * 基底クラス（継承元クラス）と重複する属性については、基底クラスのコンストラクタを呼び出す。
+	 * このクラス（継承クラス）に特化した属性だけを初期化する。
+	 */
+	function __construct(string $name, int $height, int $builtYear) {
+		parent::__construct($name, $height);
+		$this->builtYear = $builtYear;
+	}
+
+	/**
+	 * アクセサメソッド
+	 * 基底クラスと重複する属性のアクセサメソッドは定義しない（基底クラスのアクセサメソッドがそのまま利用できる）。
+	 * このクラスに特化した属性のアクセッサメソッドを定義する。
+	 */
+	function setBuiltYear(int $year): void {
+		$this->builtYear = $year;
+	}
+
+	function getBuiltYear():int {
+		return $this->builtYear;
+	}
+
+	/**
+	 * タワーの情報を取得する。
+	 * @return string 諸元文字列「〈建物名〉の高さは〈建物の高さ〉mで、〈開業年〉年に開業しました。」
+	 */
+	function getProfile():string {
+		$profile = "{$this->name} の高さは {$this->height} mで、{$this->getProfile()} 年に開業しました。";
+		return $profile;
+	}
+
+}
+?>
