297344224c0d706f046befa1abcb1b4d56b413d2
wpt1005 <wpt1005@ccg.ac.jp>
2019-11-18 23:13:50 +0900

    edit user auth

---
+++ ccmc-04/user_auth/login.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+	<meta charset="UTF-8">
+	<title>ccmc-04</title>
+	<link rel="stylesheet" href="../../assets/css/style.css" />
+	<link rel="stylesheet" href="../../assets/css/ccmc-03.css" />
+	<link rel="stylesheet" href="../../assets/css/ccmc-04.css" />
+</head>
+<body>
+	<h1>簡単なユーザ認証</h1>
+	<p>ようこそ、usr01さん</p>
+	<p><a href="logout.html">ログアウトする</a></p>
+</body>
+</html>
