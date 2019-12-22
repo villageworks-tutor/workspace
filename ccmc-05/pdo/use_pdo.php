a531f121b2b23e06cc6f9911d9c11db4024f385e
wpt1005 <wpt1005@ccg.ac.jp>
2019-12-22 22:48:16 +0900

    change pdo arguments

--- ccmc-05/pdo/use_pdo.php
+++ ccmc-05/pdo/use_pdo.php
@@ -10,7 +10,7 @@ try {
 	// データベース接続オブジェクトを取得
 	$options = array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET CHARACTER SET 'utf8'");
 	// PDOの呼び出し
-	$pdo = new PDO($dns, $user, $password, $options);
+	$pdo = new PDO($dsn, $user, $password, $options);
 	$pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
 	echo $e->getMessage();
