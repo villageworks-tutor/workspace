f93deb425eeca5f25787d6556ecf960b8a4471da
villageworks-tutor <villageworks-tutor@gamil.com>
2020-02-03 05:48:59 +0000

    2020-02-03 Mon.

--- ccmc-05/pdo/pages/restaurants.php
+++ ccmc-05/pdo/pages/restaurants.php
@@ -1,3 +1,35 @@
+<?php
+// 外部ファイルの読込み
+require_once("../database.php");
+require_once("../classes.php");
+// リクエストパラメータを取得
+$area = -1;
+if (isset($_REQUEST["area"])) {
+  $area = $_REQUEST["area"];
+}
+// データベースに接続
+$pdo = connectDatabase();
+// 実行するSQLを設定（SQLはプレースホルダ付き）
+$sql = "select * from restaurants where area=?";
+// SQL実行オブジェクトを取得
+$pstmt = $pdo->prepare($sql);
+// プレースホルダにリクエストラメータを設定
+$pstmt->bindValue(1, $area);
+// SQLを実行
+$pstmt->execute();
+// 結果セットを取得
+$rs = $pstmt->fetchAll();
+// 結果セットを配列に格納
+$restaurants = [];
+foreach ($rs as $record) {
+  $id = intval($record["id"]);
+  $name = $record["name"];
+  $detail = $record["detail"];
+  $image = $record["image"];
+  $restaurant = new Restaurant($id, $name, $detail, $image, $area);
+  $restaurants[] = $restaurant;
+}
+?>
 <!DOCTYPE html>
 <html lang="ja">
 	<head>
@@ -15,28 +47,15 @@
 				<th>画像ファイル名</th>
 				<th>地域ID</th>
 			</tr>
-						<tr>
-				<td>1</td>
-				<td>Wine Bar ENOTECA</td>
-				<td>常時10種類以上の赤・白ワインをご用意しています。
-記念日にご来店ください。</td>
-				<td>restaurant_1.jpg</td>
-				<td>2</td>
+			<?php foreach ($restaurants as $restaurant) { ?>
+			<tr>
+				<td><?= $restaurant->getId() ?></td>
+				<td><?= $restaurant->getName() ?></td>
+				<td><?= $restaurant->getDetail() ?></td>
+				<td><?= $restaurant->getImage() ?></td>
+				<td><?= $restaurant->getArea() ?></td>
 			</tr>
-						<tr>
-				<td>4</td>
-				<td>レストラン「有閑」</td>
-				<td>広い店内で、お昼の優雅なひと時を過ごしませんか？</td>
-				<td>restaurant_4.jpg</td>
-				<td>2</td>
-			</tr>
-						<tr>
-				<td>6</td>
-				<td>海沿いのレストラン La Mer</td>
-				<td>海が見える、海沿いのレストランです。</td>
-				<td>restaurant_6.jpg</td>
-				<td>2</td>
-			</tr>
-					</table>
+			<?php } ?>
+		</table>
 	</body>
 </html>
